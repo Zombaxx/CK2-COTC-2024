69661009b1ab4e2424df06ca60b98e1dd7dff008
Zombaxx <58960507+Zombaxx@users.noreply.github.com>
2024-10-15 17:40:04 -0700

    Integrated reformation through the Your Custom Heresy mod
    Updated localization from new integration to be about ideological revolution rather than heresy

---
+++ confederation at crisis/common/defines/trex_heresy_defines.lua
@@ -0,0 +1,9 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NReligion.RETINUE_FROM_REALMSIZE = 3.0
+-- NDefines.NMilitary.RETINUE_REINFORCE_COST = 2.5
+
+
+NDefines.NReligion.REFORM_RELIGION_MIN_AUTHORITY = 0.4
+NDefines.NReligion.REFORM_RELIGION_MIN_HOLY_SITES = 1
+NDefines.NReligion.REFORM_RELIGION_PIETY_COST = 2000
